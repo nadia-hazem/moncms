cac83db178ce5020775465afde6ded21f74fd26b
nedh <nedh.design@gmail.com>
2022-12-21 14:39:47 +0100

    changement de thème suite buggs

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+  /* Ajoute la feuille de style du thème parent */
+  add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
+  function wpchild_enqueue_styles(){
+    wp_enqueue_style( 'ns-theme-style', get_template_directory_uri() . '/style.css' );
+  }
