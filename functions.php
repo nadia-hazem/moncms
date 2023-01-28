13eb42a619b5c3fac1dc3c227ce3b32447d98236
Pictelle <nadia.hazem@laplateforme.io>
2023-01-28 11:48:58 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,32 @@
 <?php
 
-  /* Ajoute la feuille de style du thème parent */
-  add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
-  function wpchild_enqueue_styles(){
-    wp_enqueue_style( 'ns-theme-style', get_template_directory_uri() . 'style.css' );
-  }
+// ACTIVTATION DU THEME ENFANT
+
+function theme_enqueue_styles() {
+ wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+// SUPPRESSION DU NUMERO DE VERSION DE WORDPRESS
+
+function dc_delete_version() {
+  return '';
+}
+add_filter('the_generator', 'dc_delete_version');
+
+
+// AJOUT DES ICONES FONT AWESOME
+
+function dc_load_fontawesome() {
+  wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', null, '4.7.0' );
+}
+add_action('wp_enqueue_scripts', 'dc_load_fontawesome');
+
+
+// MASQUER LES ERREURS DE CONNEXION A L'ADMINISTRATION
+
+function wpm_hide_errors() {
+	return "L'identifiant ou le mot de passe est incorrect";
+}
+add_filter('login_errors', 'wpm_hide_errors');
