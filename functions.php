3c38c6a06a4368a046d38c05f73fddc2eb9ae489
nedh <nedh.design@gmail.com>
2023-01-28 11:43:58 +0100

    ajout fonts

--- functions.php
+++ functions.php
@@ -3,5 +3,5 @@
   /* Ajoute la feuille de style du thème parent */
   add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
   function wpchild_enqueue_styles(){
-    wp_enqueue_style( 'ns-theme-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'ns-theme-style', get_template_directory_uri() . 'style.css' );
   }
